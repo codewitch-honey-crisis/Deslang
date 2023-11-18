6079f80e3bd290652b3f5c3f523f77294d146b1d
codewitch-honey-crisis <gaztoast@gmail.com>
2023-11-18 03:54:36 -0800

    Add project files.

---
+++ DeslangDemo/Widget.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CorporateHellscape
+{
+	/// <summary>
+	/// Base widget implementation
+	/// </summary>
+	partial class Widget 
+	{
+		// our payload - to be filled
+		byte[] _payload;
+	
+		public override string ToString()
+		{
+			return string.Concat("[Widget - " , Convert.ToBase64String(_payload) , "]");
+		}
+	}
+}
